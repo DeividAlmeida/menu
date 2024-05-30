04cd56593f8b062adff5b9985056f3f42164c3ce
Deivid Almeida <mr.davialmeida@hotmail.com>
2024-05-30 18:53:31 -0300

    building open cart

--- src/cart/cart.tsx
+++ src/cart/cart.tsx
@@ -1,8 +1,43 @@
 import "./cart.css";
 export const Cart = () => {
   return (
-    <div className="cart">
-     
-    </div>
+    <section className="cart">
+      <div className="container">
+        <h1 className="cart-title">Seu Pedido</h1>
+        <details className="cart-item" open>
+          <summary className="item-header">
+              <button className="fa-trash ">lixo</button>
+              <h2 className="item-name">Pizza Media</h2>
+            <div className="item-header">
+            </div>
+          </summary>
+          <div className="item-body">
+            <div className="content">
+              <div className="item-info">
+                <p className="item-taste">Pepperoni Pizza</p>
+                <p className="item-remove">x</p>
+              </div>
+              <div className="item-info">
+                <p className="item-taste">Pepperoni Pizza</p>
+                <p className="item-remove">x</p>
+              </div>
+              <div className="item-info">
+                <p className="item-taste">Pepperoni Pizza</p>
+                <p className="item-remove">x</p>
+              </div>
+              
+            </div>
+          </div>
+        </details>
+        <div className="sub-total">
+        <p className="sub-total-lable">
+            Sub-total
+          </p>
+          <p className="sub-total-value">
+            R$ 30,00
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
