c7d30dddb07213ef4ba1955560343c1f2da60f03
Deivid Almeida <mr.davialmeida@hotmail.com>
2024-05-31 18:27:45 -0300

    add total price

--- src/cart/cart.tsx
+++ src/cart/cart.tsx
@@ -28,15 +28,45 @@ export const Cart = () => {
               
             </div>
           </div>
+          <div className="item-footer">
+            <div className="content">
+              <div className="item-info">
+                <p className="item-complement">
+                  Borda de cheddar
+                  <p className="item-complement-price">R$ 5,00</p>
+                </p>
+                  <p className="item-remove">x</p>
+              </div>
+            </div>
+          </div>
         </details>
         <div className="sub-total">
         <p className="sub-total-lable">
             Sub-total
           </p>
-          <p className="sub-total-value">
+          <p className="sub-total-price">
             R$ 30,00
           </p>
         </div>
+
+        <div className="total-price">
+          <div className="price-box">
+            <span className="total-price-label">Sub-Total</span>
+            <strong className="total-price-value">R$ 35,00</strong>
+          </div>
+          <div className="price-box">
+            <span className="total-price-label">Taxa de entrega</span>
+            <strong className="total-price-value">R$ 35,00</strong>
+          </div>
+          <div className="price-box">
+            <span className="total-price-label">Total</span>
+            <strong className="total-price-value">R$ 35,00</strong>
+          </div>
+        </div>
+
+        {/* <div className="form-floating">
+          <textarea className="form-control" placeholder="Comentário" ></textarea>
+        </div> */}
       </div>
     </section>
   );
