f21c5808db8cc6e6863c00399a8666d4fc2c4bb3
FilipeRangel08 <FilipeRangelM08@gmail.com>
2024-06-04 11:07:16 -0300

    style details 1.4

--- src/App.tsx
+++ src/App.tsx
@@ -285,6 +285,25 @@ const Home = () => {
           ))}
         </div>
       </div>
+      <nav className="nav-bar-down">
+        
+          <a className='nav-btn' href="/">
+            <i className="fa-solid fa-house"></i>
+            Início
+          </a>
+        
+          <a className='nav-btn' href="/my-order">
+            <i className="fa-solid fa-clipboard-list"></i>
+            Pedidos
+          </a>
+        
+          <a className='nav-btn' href="/cart">
+            <i className="fa-solid fa-cart-shopping"></i>
+            Carrinho
+          </a>
+
+          <div className="animation-nav-btn start-home"></div>
+      </nav>
       <footer>
         <p>&copy; 2024 Gui Pizza</p>
       </footer>
