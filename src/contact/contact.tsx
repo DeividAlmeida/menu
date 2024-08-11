d01ab55c452ac3c770bd77f81fd7a6cf58cffcc6
Deivid Almeida <mr.davialmeida@hotmail.com>
2024-08-10 23:17:10 -0300

    add redirect

--- src/contact/contact.tsx
+++ src/contact/contact.tsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
  export const Contact = () => {
   let { number } = useParams();
-  window.open(`https://wa.me/55${number}`);
+  window.location.href = `https://wa.me/55${number}`;
   return null;
  };
